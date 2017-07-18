a46418745d287f07ed202553577486f03a7939ed
Helena Purgatorio <helena@us.ibm.com>
2017-07-18 18:26:47 -0500

    Added back in support for AFU_CMD_INTERRUPT_D

--- ocse/cmd.c
+++ ocse/cmd.c
@@ -296,7 +296,7 @@ static void _add_cmd(struct cmd *cmd, uint32_t context, uint32_t afutag,
 // Format and add interrupt to command list
 
  static void _add_interrupt(struct cmd *cmd, uint16_t actag, uint16_t afutag, 
- 			   uint8_t cmd_opcode, uint8_t *cmd_ea_or_obj, uint8_t cmd_flag) 
+ 			   uint8_t cmd_opcode, uint8_t *cmd_ea_or_obj, uint16_t size, uint8_t cmd_data_is_valid, uint8_t cmd_flag) 
  { 
  	//uint32_t resp = TLX_RSP_INTRP_RESP; 
  	uint32_t resp= 0; //FOR NOW, always a good response
@@ -311,11 +311,11 @@ static void _add_cmd(struct cmd *cmd, uint32_t context, uint32_t afutag,
  
         // setting MEM_IDLE will tell handle_interrupt to send req to libocxl 
         if (cmd_opcode == AFU_CMD_WAKE_HOST_THRD)
-		_add_cmd(cmd, context, afutag, cmd_opcode, CMD_WAKE_HOST_THRD, addr, 0, MEM_IDLE,
-		 resp, 0, 0, 0, 0, 0);
+		_add_cmd(cmd, context, afutag, cmd_opcode, CMD_WAKE_HOST_THRD, addr, size, MEM_IDLE,
+		 resp, 0, cmd_data_is_valid, 0, cmd_flag, 0);
 	else  //must be some type of INTR request
-		_add_cmd(cmd, context, afutag, cmd_opcode, CMD_INTERRUPT, addr, 0, MEM_IDLE,
-		 resp, 0, 0, 0, 0, 0);
+		_add_cmd(cmd, context, afutag, cmd_opcode, CMD_INTERRUPT, addr, size, MEM_IDLE,
+		 resp, 0, cmd_data_is_valid, 0, cmd_flag, 0);
  } 
 
 // Format and add misc. command to list
@@ -624,12 +624,15 @@ static void _parse_cmd(struct cmd *cmd,
 			  cmd_ea_or_obj, cmd_pl, cmd_data_is_valid, cmd_flag, cmd_endian);
 		break;
 		// Interrupt
+	case AFU_CMD_INTRP_REQ_D: // not sure POWER supports this one?
+		debug_msg("YES! AFU cmd is  INTRPT REQ WITH DATA\n");
+		_add_interrupt(cmd, cmd_actag, cmd_afutag, cmd_opcode,
+			  cmd_ea_or_obj, pl_to_size( cmd_pl), cmd_data_is_valid, cmd_flag);
 	case AFU_CMD_INTRP_REQ:
-	//case AFU_CMD_INTRP_REQ_D: // not sure POWER supports this one?
 	case AFU_CMD_WAKE_HOST_THRD:
 		debug_msg("YES! AFU cmd is either INTRPT REQ or WAKE HOST THREAD\n");
 		_add_interrupt(cmd, cmd_actag, cmd_afutag, cmd_opcode,
-			  cmd_ea_or_obj, cmd_flag);
+			  cmd_ea_or_obj, 0, cmd_data_is_valid, cmd_flag);
 	// TODO what about stream_id ?
 		break;
 	case AFU_CMD_NOP:
@@ -1195,7 +1198,9 @@ void handle_interrupt(struct cmd *cmd)
 	struct cmd_event *event;
 	struct client *client;
 	// uint16_t irq;
-	uint8_t buffer[10];
+	uint64_t offset;
+	uint16_t byte_count;
+	uint8_t buffer[45];
 
 	// Make sure cmd structure is valid
 	if (cmd == NULL)
@@ -1219,14 +1224,23 @@ void handle_interrupt(struct cmd *cmd)
 	// Send interrupt or wake_host_thread request to client
 	if (event->type == CMD_WAKE_HOST_THRD)
 		buffer[0] = OCSE_WAKE_HOST_THREAD;
-	else //must be an interrupt
-		buffer[0] = OCSE_INTERRUPT;
+	else if (event->command == AFU_CMD_INTRP_REQ_D) 
+			buffer[0] = OCSE_INTERRUPT_D;
+		else
+			buffer[0] = OCSE_INTERRUPT;
 
 	//irq = htons(cmd->irq);
 	//memcpy(&(buffer[1]), &irq, 2);
 	memcpy(&(buffer[1]), &event->cmd_flag, 1);
 	memcpy(&(buffer[2]), &event->addr, 8);
-
+	byte_count = 10;
+	if (event->command == AFU_CMD_INTRP_REQ_D) {
+		offset = event->addr & ~CACHELINE_MASK;
+		memcpy(&(buffer[10]), &event->size, 2);
+		byte_count += 2;
+		memcpy(&(buffer[12]), &(event->data[offset]), event->size);
+		byte_count += event->size;
+	}
 	// do we still need this event->abort???
 	event->abort = &(client->abort);
 	debug_msg( "%s:INTERRUPT cmd=0x%02x cmd_flag=%d addr=0x%016"PRIx64, 
@@ -1234,7 +1248,7 @@ void handle_interrupt(struct cmd *cmd)
 		   event->command, 
 		   event->cmd_flag, 
 		   event->addr );
-	if (put_bytes(client->fd, 10, buffer, cmd->dbg_fp, cmd->dbg_id,
+	if (put_bytes(client->fd, byte_count, buffer, cmd->dbg_fp, cmd->dbg_id,
 		      event->context) < 0) {
 		client_drop(client, TLX_IDLE_CYCLES, CLIENT_NONE);
 	}
